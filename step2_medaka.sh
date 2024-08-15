a3cbf8bdf8eb28ab759db8fde24fcb6401da4275
luanelandau <145043266+luanelandau@users.noreply.github.com>
2024-08-15 15:47:08 -0400

    Update step2_medaka.sh

--- step2_medaka.sh
+++ step2_medaka.sh
@@ -12,6 +12,8 @@
 #SBATCH --constraint=AVX512
 #SBATCH --export=NONE
 
+#medaka (https://github.com/nanoporetech/medaka) is a tool that allows you to polish your draft assembly using your own reads. It is an important step into polishing the assembly. 
+
 #all these modules were load while troubleshooting for some problems with the script. They do not necessarily have to be loaded but I will leave them here, because it is working.
 module load CCRconfig
 module load gcc/11.2.0
@@ -21,12 +23,12 @@ module load gcccore/11.2.0
 module load samtools/1.16.1
 
 eval "$(/projects/academic/omergokc/own_modules/2023.01/2023.01/software/Core/anaconda3/2023.03-1/bin/conda shell.bash hook)" #this is to load conda
-conda activate Assembly #this is the environment in which I have medaka installed. For more information look into: https://github.com/nanoporetech/medaka
+conda activate Assembly #this is the environment in which I have medaka installed. 
 #For running this I have to have installed minimap2 and samtools!
 
 #input
-reads="path/to/your/reads.fastq.gz" #path to fastq files
-assembly="assembly.fasta" #path to draft genome
+reads="path/to/your/reads.fastq.gz" #path to fastq files with raw reads
+assembly="assembly.fasta" #path to draft genome, in this case the one I generated with flye
 sample="HG01946_10kbp_flye" #sample name
 model="r1041_e82_400bps_sup_g615" #basecalling model used for fastq files
 mapthreads=56 #Mapping threads
@@ -35,7 +37,7 @@ conthreads=8 #compiling threads Creators of Medaka say over 2 is not useful but 
 #mkdir results
 
 #align reads to assembly
-#mini_align -i ${reads} -r ${assembly} -P -m -f -p ./results/${sample}_draft -t ${mapthreads}
+mini_align -i ${reads} -r ${assembly} -P -m -f -p ./results/${sample}_draft -t ${mapthreads}
 
 # you can run lots of jobs like this at the same time by using the --region flag and providing a list of a subset of the contigs found in you draft assembly.
 medaka consensus results/${sample}_draft.bam results/${sample}_new2.hdf --model ${model} --threads ${conthreads}
