d117a3f63b5ac668d0ed81204bf35080bcddf1aa
luanelandau <145043266+luanelandau@users.noreply.github.com>
2024-10-28 12:44:15 -0400

    Update hifiasm_ULR.sh

--- hifiasm_ULR.sh
+++ hifiasm_ULR.sh
@@ -6,9 +6,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --mem=600G
-#SBATCH --job-name=1922_hifiasm
-#SBATCH --output=1922_hifiasm.out
-#SBATCH --error=1922_hifiasm.err
+#SBATCH --job-name=1972_hifiasm
+#SBATCH --output=1972_hifiasm.out
+#SBATCH --error=1972_hifiasm.err
 #SBATCH --mail-user=luanejan@buffalo.edu
 #SBATCH --mail-type=ALL
 ##SBATCH --requeue
@@ -17,33 +17,24 @@ module load gcccore/11.2.0
 module load hifiasm/0.19.6
 module load seqkit/2.7.0
 
-cor_reads="/projects/academic/omergokc/Luane/HG01922/herro/HG01922_10kbp_HERRO.fasta"
-#par1="/projects/academic/omergokc/Luane/HG01922/verkko/diploid/HG01920.fastq.gz"
-#par2="/projects/academic/omergokc/Luane/HG01922/verkko/diploid/HG01921.fastq.gz"
-raw="/projects/academic/omergokc/Luane/HG01922/HG01922_allreads.fastq.gz"
+cor_reads="/projects/academic/omergokc/Luane/HG01972/herro/HG01972_10kbp_HERRO.fasta"
+#par1="/projects/academic/omergokc/Luane/HG01972/verkko/HG01971.fastq.gz"
+#par2="/projects/academic/omergokc/Luane/HG01972/verkko/HG01970.fastq.gz"
 
-out_pre="HG01922"
+out_pre="HG01972"
 
-#If coverage is higher than 38x, then the reads need to be downsampled
-#Corrected reads were first chopped to a length of 30,000 bp, and chunks shorter than 10,000 bp were filtered away using SeqKit v2.5.1:
-#seqkit sliding -s 30000 -W 30000 -g ${cor_reads} > chopped.fasta
-#seqkit seq -m 10000 chopped.fasta > processed.fasta
+raw="/projects/academic/omergokc/Luane/HG01972/HG01972_allreads.fastq.gz"
 
-
-#If coverage is higher than 38x, then the reads need to be downsampled
-#Corrected reads were first chopped to a length of 30,000 bp, and chunks shorter than 10,000 bp were filtered away using SeqKit v2.5.1:
-#seqkit sliding -s 30000 -W 30000 -g ${cor_reads} > chopped.fasta
-#seqkit seq -m 10000 chopped.fasta > processed.fasta
 
 #October 25
-seqkit seq -m 50000 $raw > raw_reads_HG01922_50kb.fastq.gz
+seqkit seq -m 50000 $raw > raw_reads_HG01972_50kb.fastq.gz
 
-seqkit seq -m 100000 $raw > raw_reads_HG01922_100kb.fastq.gz
+seqkit seq -m 100000 $raw > raw_reads_HG01972_100kb.fastq.gz
 
-par1="/projects/academic/omergokc/Luane/HG01922/hifiasm/diploid/parent1.yak"
-par2="/projects/academic/omergokc/Luane/HG01922/hifiasm/diploid/parent2.yak"
+par1="/projects/academic/omergokc/Luane/HG01972/hifiasm/phased/parent1.yak"
+par2="/projects/academic/omergokc/Luane/HG01972/hifiasm/phased/parent2.yak"
 
 
-/projects/academic/omergokc/Luane/softwares/hifiasm/hifiasm -t 32 -o HG01946_diploid_50kb --ul raw_reads_HG01946_50kb.fastq --ul-cut 50000 --dual-scaf -1 $par1 -2 $par2 $cor_reads 
+/projects/academic/omergokc/Luane/softwares/hifiasm/hifiasm -t 32 -o HG01946_diploid_50kb --ul raw_reads_HG01972_50kb.fastq.gz --ul-cut 50000 --dual-scaf -1 $par1 -2 $par2 $cor_reads 
 
-/projects/academic/omergokc/Luane/softwares/hifiasm/hifiasm -t 32 -o HG01946_diploid_100kb --ul raw_reads_HG01946_100kb.fastq --ul-cut 100000 --dual-scaf -1 $par1 -2 $par2 $cor_reads   
+/projects/academic/omergokc/Luane/softwares/hifiasm/hifiasm -t 32 -o HG01946_diploid_100kb --ul raw_reads_HG01972_100kb.fastq.gz --ul-cut 100000 --dual-scaf -1 $par1 -2 $par2 $cor_reads   
